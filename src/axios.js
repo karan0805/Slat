db03012d1822443dfa54c6207e013812d55293cd
karan <61943065+karan0805@users.noreply.github.com>
2022-02-17 18:42:29 +0530

    :gear: edited env

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_BASEURL,
+  baseURL: process.env.REACT_APP_BACKENDURL,
   timeout: 10000,
   headers: {
     Authorization: window.localStorage.getItem('access_token'),

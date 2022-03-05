0a6b04f1c6d10243b933abfde349a858344ea784
Sanket <sanketmense@gmail.com>
2022-03-05 16:27:25 +0530

    create board

--- src/pages/ProjectView/components/Board.js
+++ src/pages/ProjectView/components/Board.js
@@ -1,5 +1,56 @@
 import React from 'react';
+import { SimpleGrid, Box, Group, ThemeIcon, Text, Avatar } from '@mantine/core';
+import { BiPlus } from 'react-icons/bi';
 
 export const Board = () => {
-  return <div>Board</div>;
+  return (
+    <>
+      <SimpleGrid cols={4}>
+        <Box
+          sx={() => ({
+            display: 'block',
+            cursor: 'pointer',
+            padding: '5px',
+            borderRadius: '5px',
+            fontWeight: '500',
+
+            '&:hover': {
+              backgroundColor: '#f5f5f5',
+            },
+          })}
+        >
+          <Group>
+            <ThemeIcon
+              variant="light"
+              size="lg"
+              color="pink"
+              style={{ border: '1px dashed black' }}
+            >
+              <BiPlus />
+            </ThemeIcon>
+            <Text>Create Project</Text>
+          </Group>
+        </Box>
+
+        <Box
+          sx={() => ({
+            display: 'block',
+            cursor: 'pointer',
+            padding: '5px',
+            borderRadius: '5px',
+            fontWeight: '500',
+
+            '&:hover': {
+              backgroundColor: '#f5f5f5',
+            },
+          })}
+        >
+          <Group>
+            <Avatar radius="md" size="32px" />
+            <Text>YO</Text>
+          </Group>
+        </Box>
+      </SimpleGrid>
+    </>
+  );
 };

3664680f4408373dabc5dc6a90c9e8bfad4d53c9
kkhitesh <hkowdiki@gmail.com>
2022-03-29 20:37:00 +0530

    :writing_hand: Minor Changes

--- src/pages/Explore/index.js
+++ src/pages/Explore/index.js
@@ -1,98 +1,122 @@
 import React from 'react';
 import { Table } from '@mantine/core';
+import { Badge } from '@mantine/core';
 import { useNavigate } from 'react-router-dom';
+import './index.css';
+import { useSelector } from 'react-redux';
+import { selectActiveOrgDetails } from '../../redux/slices/OrgSlice';
 
 const Explore = () => {
-  const projectData = [
-    {
-      id: 1,
-      name: 'Slat',
-      endDate: '2022-03-05',
-      status: 'In Progress',
-      lead: 'John Doe',
-      members: [
-        {
-          name: 'John Doe',
-          title: 'Lead',
-          children: [
-            {
-              name: 'Jane Doe',
-              title: 'Maintainer',
-              children: [
-                {
-                  name: 'Mark Doe',
-                  title: 'Member',
-                },
-                {
-                  name: 'Mary Doe',
-                  title: 'Member',
-                },
-              ],
-            },
-            {
-              name: 'Scott Doe',
-              title: 'Maintainer',
-              children: [
-                {
-                  name: 'Drake Doe',
-                  title: 'Member',
-                },
-              ],
-            },
-            {
-              name: 'Tom Doe',
-              title: 'Maintainer',
-              children: [
-                {
-                  name: 'Jack Doe',
-                  title: 'Member',
-                },
-              ],
-            },
-          ],
-        },
-      ],
-    },
-    {
-      id: 2,
-      name: 'Query',
-      endDate: '2022-05-03',
-      status: 'Not Started',
-      lead: 'Charlie',
-      members: [
-        {
-          name: 'Charlie',
-          title: 'Lead',
-          children: [],
-        },
-      ],
-    },
-    {
-      id: 3,
-      name: 'Algo',
-      endDate: '2022-02-05',
-      status: 'Completed',
-      lead: 'Alan',
-    },
-  ];
+  // const projectData = [
+  //   {
+  //     id: 1,
+  //     name: 'Slat',
+  //     endDate: '2022-03-05',
+  //     status: 'In Progress',
+  //     lead: 'John Doe',
+  //     members: [
+  //       {
+  //         name: 'John Doe',
+  //         title: 'Lead',
+  //         children: [
+  //           {
+  //             name: 'Jane Doe',
+  //             title: 'Maintainer',
+  //             children: [
+  //               {
+  //                 name: 'Mark Doe',
+  //                 title: 'Member',
+  //               },
+  //               {
+  //                 name: 'Mary Doe',
+  //                 title: 'Member',
+  //               },
+  //             ],
+  //           },
+  //           {
+  //             name: 'Scott Doe',
+  //             title: 'Maintainer',
+  //             children: [
+  //               {
+  //                 name: 'Drake Doe',
+  //                 title: 'Member',
+  //               },
+  //             ],
+  //           },
+  //           {
+  //             name: 'Tom Doe',
+  //             title: 'Maintainer',
+  //             children: [
+  //               {
+  //                 name: 'Jack Doe',
+  //                 title: 'Member',
+  //               },
+  //             ],
+  //           },
+  //         ],
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     id: 2,
+  //     name: 'Query',
+  //     endDate: '2022-05-03',
+  //     status: 'Not Started',
+  //     lead: 'Charlie',
+  //     members: [
+  //       {
+  //         name: 'Charlie',
+  //         title: 'Lead',
+  //         children: [],
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     id: 3,
+  //     name: 'Algo',
+  //     endDate: '2022-02-05',
+  //     status: 'Completed',
+  //     lead: 'Alan',
+  //   },
+  // ];
+
+  const projectData = useSelector(selectActiveOrgDetails);
 
   const nav = useNavigate();
 
-  const projects = projectData.map((project) => (
+  const statusBadge = (status) => {
+    if (status === 'In Progress') {
+      return <Badge size="lg">{status}</Badge>;
+    } else if (status === 'Completed') {
+      return (
+        <Badge color="green" size="lg">
+          {status}
+        </Badge>
+      );
+    } else {
+      return (
+        <Badge color="red" size="lg">
+          {status}
+        </Badge>
+      );
+    }
+  };
+
+  const projects = projectData.projects.map((project) => (
     <tr
       key={project.id}
       onClick={() => nav('/dashboard/project/chart/' + project.id)}
     >
       <td>{project.name}</td>
-      <td>{project.endDate}</td>
-      <td>{project.status}</td>
+      <td>{project.endDate.split('T')[0]}</td>
+      <td>{statusBadge(project.status)}</td>
       <td>{project.lead}</td>
     </tr>
   ));
 
   return (
     <>
-      <div align="center">
+      <div align="center" className="table">
         <h1> 🔍 Explore</h1>
         <p>Have a look at all the Projects within the Organization</p>
         <Table horizontalSpacing="xl" verticalSpacing="xs" highlightOnHover>

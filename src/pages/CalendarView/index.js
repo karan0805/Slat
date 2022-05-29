f75846baa064d33b581c9e336f9f1ea1d623daae
Karan Gandhi <karangandhi.navsari@gmail.com>
2022-05-29 16:08:02 +0530

    events

--- src/pages/CalendarView/index.js
+++ src/pages/CalendarView/index.js
@@ -1,30 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { Button } from '@mantine/core';
 import { BsPlusCircle } from 'react-icons/bs';
 import CreateEvent from '../../components/CreateEvent';
+import { orgApi } from '../../api';
+import { useSelector } from 'react-redux';
+import { selectActiveOrg } from '../../redux/slices/OrgSlice';
 
 const localizer = momentLocalizer(moment);
 
 const CalendarView = () => {
-  const [addEvent, setAddEvent] = React.useState(false);
+  const [addEvent, setAddEvent] = useState(false);
+  const activeOrg = useSelector(selectActiveOrg);
+  const [call, setCall] = useState(false);
+  const [events, setEvents] = useState([]);
 
-  const events = [
-    {
-      id: 13,
-      title: 'HireMe Development',
-      start: new Date(2022, 4, 3, 0, 0, 0),
-      end: new Date(2022, 4, 10, 2, 0, 0),
-    },
-  ];
+  useEffect(() => {
+    orgApi.getEvents(activeOrg).then((res) => {
+      if (res.status === 200) {
+        console.log(res.data.data);
+        setEvents(res.data.data);
+        setCall(false);
+      }
+    });
+  }, [call]);
+
   return (
     <div style={{ height: '400pt', width: '800pt', margin: '50px auto' }}>
       <CreateEvent
         addEvent={addEvent}
         setAddEvent={setAddEvent}
         events={events}
+        activeOrg={activeOrg}
+        setCall={setCall}
       />
       <Calendar
         events={events}
